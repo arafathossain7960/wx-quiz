42c5f90f0556aa757f2d0222a7db0735aecae61b
Arafat Hossaine <arafathossain7960@gmail.comm>
2022-11-28 08:29:11 +0600

    complete footer

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <div>
-            <h4>This is a footer section </h4>
+        <div className='p-16 mt-4 bg-blue-100 text-center'>
+            <h4 className='text-lg'><b>WX-quiz  </b> is a programming skill test platform</h4><p className='text-sm text-black-900'>All copy right 2022 reserved by <i>WX-quiz</i> </p>
         </div>
     );
 };
